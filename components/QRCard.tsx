d82a4c0e99713de5faa9b6d516a645691a41321a
adrianahzm <adriiana.guzman@hotmail.com>
2025-01-02 11:47:58 -0600

    Update size card

--- components/QRCard.tsx
+++ components/QRCard.tsx
@@ -3,20 +3,20 @@ import Image from "next/image";
 const QRCard = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-slate-300">
-      <div className="bg-white rounded-2xl shadow-lg p-4 w-60 sm:w-77">
+      <div className="bg-white rounded-2xl shadow-lg p-6 w-72 sm:w-80"> 
         <div className="flex justify-center">
           <Image
             src="/image-qr-code.png"
             alt="QR Code"
-            width={200}
-            height={200}
+            width={240}  
+            height={240} 
             className="rounded-lg"
           />
         </div>
-        <h1 className="text-slate-900 text-lg font-bold text-center mt-6 font-outfit">
+        <h1 className="text-slate-900 text-xl font-bold text-center mt-8 font-outfit"> {/* Aumenté el tamaño de la fuente */}
           Improve your front-end skills by building projects
         </h1>
-        <p className="text-slate-500 text-center mt-4 font-outfit text-[15px]">
+        <p className="text-slate-500 text-center mt-6 font-outfit text-[16px]"> {/* Aumenté el tamaño de la fuente */}
           Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
         </p>
       </div>
