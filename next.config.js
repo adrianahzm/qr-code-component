591e57dfcc9e8baef7c36387ed70ea8de3b55a9e
adrianahzm <adriiana.guzman@hotmail.com>
2025-01-02 11:06:03 -0600

    Fix tailwind error

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   basePath: isProd ? '/qr-code-component' : '',
   output: 'export',
-  distDir: 'dist',
   images: {
     unoptimized: true,
   }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,12 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx}', // Para Next.js
+    './components/**/*.{js,ts,jsx,tsx}', // Para tus componentes
+    './src/**/*.{js,ts,jsx,tsx}', // Para Vite o React
+    './public/index.html', // Para HTML estático
   ],
+  
   theme: {
     extend: {
       colors: {
